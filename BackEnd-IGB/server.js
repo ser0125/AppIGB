3c8e5b94799c9c5871a984444a951748fde0f9e6
ser0125 <sergio.llanos.garcia@gmail.com>
2018-08-23 16:51:25 -0500

    Configuración inicial

---
+++ BackEnd-IGB/server.js
@@ -0,0 +1,16 @@
+// Set up
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+ 
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json()); // Send JSON responses
+app.use(logger('dev')); // Log requests to API using morgan
+app.use(cors());
+
+// listen
+app.listen(8080);
+console.log("App listening on port 8080");
